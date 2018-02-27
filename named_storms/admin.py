db87d7faf922a8bcf808d79241ceff45621d87bc
danny flack <flackattack@gmail.com>
2018-02-27 09:47:07 -0500

    inline admin (editing inline maps broken)

--- named_storms/admin.py
+++ named_storms/admin.py
@@ -2,6 +2,33 @@ from django.contrib.gis import admin
 from named_storms.models import NamedStorm, NamedStormCoveredData, NamedStormCoveredDataProvider, DataProviderProcessor
 
 
-@admin.register(NamedStorm, NamedStormCoveredData, NamedStormCoveredDataProvider, DataProviderProcessor)
-class CoveredDataAdmin(admin.GeoModelAdmin):
+class NamedStormCoveredDataInline(admin.TabularInline):
+    model = NamedStormCoveredData
+    show_change_link = True
+    extra = 0
+    readonly_fields = ('geo',)
+
+
+class NamedStormCoveredDataProviderInline(admin.TabularInline):
+    model = NamedStormCoveredDataProvider
+    show_change_link = True
+    extra = 0
+
+
+@admin.register(NamedStorm)
+class NamedStormInlineAdmin(admin.OSMGeoAdmin):
+    inlines = (
+        NamedStormCoveredDataInline,
+    )
+
+
+@admin.register(NamedStormCoveredData)
+class NamedStormCoveredDataInlineAdmin(admin.OSMGeoAdmin):
+    inlines = (
+        NamedStormCoveredDataProviderInline,
+    )
+
+
+@admin.register(NamedStormCoveredDataProvider, DataProviderProcessor)
+class NamedStormAdmin(admin.GeoModelAdmin):
     pass
