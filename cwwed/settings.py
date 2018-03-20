fc9c01884f48251054d187dd05244dede56a16ee
danny flack <flackattack@gmail.com>
2018-03-20 16:01:27 -0400

    hidden incomplete dir

--- cwwed/settings.py
+++ cwwed/settings.py
@@ -175,7 +175,7 @@ REST_FRAMEWORK = {
 # CWWED
 CWWED_DATA_DIR = '/media/bucket/cwwed'
 CWWED_COVERED_DATA_DIR_NAME = 'Covered Data'
-CWWED_COVERED_DATA_INCOMPLETE_DIR_NAME = 'incomplete'
+CWWED_COVERED_DATA_INCOMPLETE_DIR_NAME = '.incomplete'
 CWWED_COVERED_DATA_ARCHIVE_TYPE = 'gztar'
 THREDDS_URL = 'http://localhost:9000/thredds/'
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN', '')
