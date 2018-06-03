ce1eb8f41954811e0725ff933c2775e389702a01
danny flack <flackattack@gmail.com>
2018-06-03 07:47:28 -0400

    pandas filter example

--- scripts/jpl.py
+++ scripts/jpl.py
@@ -14,5 +14,8 @@ data = np.fromfile('/home/danny/Downloads/QS_L1C_57676_V1.dat', dtype=data_type)
 df = pandas.DataFrame(data)
 
 print('Rows total: {}'.format(len(df.values)))
+
+# TODO - instead, we should adjust the date we're comparing against to accommodate the year 2000 change
+# TODO - this should solve the Series int error we were seeing
 df = df[df['time'] > datetime(2018, 1, 1).timestamp()]
 print('Rows after 2018-01-01: {}'.format(len(df.values)))
