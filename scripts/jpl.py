3e4a7f739916e132bfc7582a4020f7a9001e69b6
danny flack <flackattack@gmail.com>
2018-06-05 15:03:23 -0400

    qscat L1C (wip)

--- scripts/jpl.py
+++ scripts/jpl.py
@@ -1,21 +1,45 @@
-import pandas
+import os
 from datetime import datetime
-import numpy as np
+import numpy
 
-data_type = np.dtype([
+DATE_EPOCH = datetime(2000, 1, 1)
+#DATE_CMP = datetime(2019, 1, 1)
+#DATE_CMP_TIMESTAMP = DATE_CMP.timestamp() - DATE_EPOCH.timestamp()
+BASE_DIR = '/home/danny/Desktop/v1/'
+
+# ftp://podaac.jpl.nasa.gov/allData/quikscat/L1C/sw/Python/quikscat_l1c.py
+data_type = numpy.dtype([
     ('timestr', 'S21'), ('time', 'f8'), ('lon', 'f4'), ('lat', 'f4'),
     ('fp_start', 'i4'), ('fp_end', 'i4'), ('npts', 'i4'), ('s0', 'f4'),
     ('inc', 'f4'), ('azi', 'f4'), ('atten', 'f4'), ('beam', 'u1'),
     ('land', 'u1'), ('espd', 'f4'), ('edir', 'f4'), ('rspd', 'f4'),
-])
+    ('rdir', 'f4')])
 
-data = np.fromfile('/home/danny/Downloads/QS_L1C_57676_V1.dat', dtype=data_type)
+min_date = None
+max_date = None
 
-df = pandas.DataFrame(data)
+for file in os.listdir(BASE_DIR):
+    if file.endswith('.dat'):
+        data = numpy.fromfile(os.path.join(BASE_DIR, file), dtype=data_type)
+        file_min_date = datetime.fromtimestamp(data['time'].min() + DATE_EPOCH.timestamp())
+        file_max_date = datetime.fromtimestamp(data['time'].max() + DATE_EPOCH.timestamp())
 
-print('Rows total: {}'.format(len(df.values)))
+        if min_date is None or file_min_date < min_date:
+            min_date = file_min_date
 
-# TODO - instead, we should adjust the date we're comparing against to accommodate the year 2000 change
-# TODO - this should solve the Series int error we were seeing
-df = df[df['time'] > datetime(2018, 1, 1).timestamp()]
-print('Rows after 2018-01-01: {}'.format(len(df.values)))
+        if max_date is None or file_max_date > max_date:
+            max_date = file_max_date
+
+        print('File: {}'.format(file))
+        print('File Min Stamp: {}'.format(data['time'].min()))
+        print('File Max Stamp: {}'.format(data['time'].max()))
+        print('File Min Date: {}'.format(file_min_date.isoformat()))
+        print('File Max Date: {}'.format(file_max_date.isoformat()))
+
+print('=====================')
+
+print('Min Date: {}'.format(min_date.isoformat()))
+print('Max Date: {}'.format(max_date.isoformat()))
+
+#print('Rows total: {}'.format(len(data)))
+#print('Rows filtered: {}'.format(len(data[data['time'] >= DATE_CMP_TIMESTAMP])))
