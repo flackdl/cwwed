001fd02d3f3cbf03a885d34c1657cf359ed7ba12
danny flack <flackattack@gmail.com>
2018-08-02 15:18:33 -0400

    contour (wip)

--- scripts/contours.py
+++ scripts/contours.py
@@ -4,27 +4,43 @@ import matplotlib.tri as tri
 import numpy as np
 import geojsoncontour
 
+GRID_SIZE = 1000  # make this less arbitrary
+
 dataset = xarray.open_dataset('/media/bucket/cwwed/THREDDS/PSA_demo/Sandy_DBay/DBay-run_map.nc')
 
 depths = dataset['mesh2d_waterdepth'][0]
 
-z = depths[::20][:2000]
+z = depths[::20]
 x = z.mesh2d_face_x
 y = z.mesh2d_face_y
 
-xi = np.linspace(-76, -74, 2000)
-yi = np.linspace(38, 41, 2000)
+# convert to numpy arrays
+z = z.values
+x = x.values
+y = y.values
 
+# build grid constraints
+xi = np.linspace(np.floor(x.min()), np.ceil(x.max()), GRID_SIZE)
+yi = np.linspace(np.floor(y.min()), np.ceil(y.max()), GRID_SIZE)
+
+# build delaunay triangles
 triang = tri.Triangulation(x, y)
+
+# filter out triangles (WIP)
+#mask = x[triang.triangles].max(axis=1) - x[triang.triangles].min(axis=1) > .03
+#triang.set_mask(mask)
+
+# interpolate values from triangle data and build a mesh of data
 interpolator = tri.LinearTriInterpolator(triang, z)
 Xi, Yi = np.meshgrid(xi, yi)
 zi = interpolator(Xi, Yi)
 
+# plot
 figure = plt.figure()
 ax = figure.add_subplot(111)
 contourf = ax.contourf(xi, yi, zi, cmap=plt.cm.jet)
 
-# Convert matplotlib contourf to geojson
+# convert matplotlib contourf to geojson
 gjson = geojsoncontour.contourf_to_geojson(
     contourf=contourf,
     min_angle_deg=3.0,
