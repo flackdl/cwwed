c8d8b44eeff67d707bdd3a27c056050b24f3cb1a
danny flack <flackattack@gmail.com>
2018-12-19 10:49:45 -0500

    specify number of workers and threads

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py migrate
 
 # run application
-gunicorn --worker-class gthread --bind 0.0.0.0:80 cwwed.wsgi
+gunicorn --worker-class gthread --workers 4 --threads 4 --bind 0.0.0.0:80 cwwed.wsgi
