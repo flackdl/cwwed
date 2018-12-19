b6b967f0384d63f619584392775b7af411dc151f
danny flack <flackattack@gmail.com>
2018-12-19 10:39:24 -0500

    gunicorn threads, use default worker count

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# build/migrate django database
+# migrate django database
 python manage.py migrate
 
 # run application
-gunicorn -w 4 -b 0.0.0.0:80 cwwed.wsgi
+gunicorn --worker-class gthread --bind 0.0.0.0:80 cwwed.wsgi
