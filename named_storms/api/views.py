aa678eae21bb7488165b8c31bd1a17dbaf3ce71e
danny flack <flackattack@gmail.com>
2019-04-03 16:44:11 -0400

    find nearest VALID wind index since they're not evenly distributed amongst the coordinates

--- named_storms/api/views.py
+++ named_storms/api/views.py
@@ -24,15 +24,18 @@ class PSAFilterView(views.APIView):
         # open the dataset
         ds = xr.open_dataset('/media/bucket/cwwed/OPENDAP/PSA_demo/sandy.nc')
 
+        x_coords = ds.x[:].values
+        y_coords = ds.y[:].values
+
         # create a mask "near" the coordinate to make the "nearest neighbor" faster
-        xmask = (ds.x <= coordinate[1] + .5) & (ds.x >= coordinate[1] - .5)
-        ymask = (ds.y <= coordinate[0] + .5) & (ds.y >= coordinate[0] - .5)
+        xmask = (x_coords <= coordinate[1] + .5) & (x_coords >= coordinate[1] - .5)
+        ymask = (y_coords <= coordinate[0] + .5) & (y_coords >= coordinate[0] - .5)
         mask = xmask & ymask
 
         now = time.time()
 
         # find the "nearest neighbor" node
-        nearest_index = self._nearest_node_index(ds.x, ds.y, mask, coordinate)
+        nearest_index = self._nearest_node_index(x_coords, y_coords, mask, coordinate)
 
         logging.info('Nearest (idx={}) (s): {}'.format(nearest_index, time.time() - now))
 
@@ -73,8 +76,25 @@ class PSAFilterView(views.APIView):
 
         now = time.time()
 
-        x_wind_speeds = ds.uwnd[::2, nearest_index].values
-        y_wind_speeds = ds.vwnd[::2, nearest_index].values
+        x_wind_speeds = None
+        y_wind_speeds = None
+
+        # NOTE: wind data isn't evenly distributed so we have to iteratively look for the nearest location
+        # where it has at least some values
+        while x_coords.any():
+            x_wind_speeds = ds.uwnd[::2, nearest_index].values
+            y_wind_speeds = ds.vwnd[::2, nearest_index].values
+            # has values
+            if not numpy.all(numpy.isnan(x_wind_speeds)):
+                break
+            # no values - find next nearest node
+            else:
+                x_coords = numpy.delete(x_coords, nearest_index)
+                y_coords = numpy.delete(y_coords, nearest_index)
+                xmask = (x_coords <= coordinate[1] + .5) & (x_coords >= coordinate[1] - .5)
+                ymask = (y_coords <= coordinate[0] + .5) & (y_coords >= coordinate[0] - .5)
+                mask = xmask & ymask
+                nearest_index = self._nearest_node_index(x_coords, y_coords, mask, coordinate)
 
         wind_speeds = numpy.arctan2(
             numpy.abs(x_wind_speeds),
@@ -87,15 +107,16 @@ class PSAFilterView(views.APIView):
 
         response = Response({
             'dates': dates,
-            'water_level': water_levels,
-            'wave_height': wave_heights,
-            'wind_speed': wind_speeds,
+            # convert NaN to zero
+            'water_level': numpy.nan_to_num(water_levels),
+            'wave_height': numpy.nan_to_num(wave_heights),
+            'wind_speed': numpy.nan_to_num(wind_speeds),
         })
 
         return response
 
     @staticmethod
-    def _nearest_node_index(x: xr.DataArray, y: xr.DataArray, mask: xr.DataArray, coord: tuple):
+    def _nearest_node_index(x: numpy.ndarray, y: numpy.ndarray, mask: numpy.ndarray, coord: tuple):
         coords = numpy.column_stack([y[mask], x[mask]])
         all_coords = numpy.column_stack([y, x])
         nearest = coords[spatial.KDTree(coords).query(coord)[1]]
