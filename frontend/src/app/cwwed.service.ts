6f0e255ee2a2cb3bafe7f61f13bb015f17ab2ddb
danny flack <flackattack@gmail.com>
2019-08-21 15:10:29 -0500

    use relative api paths

--- frontend/src/app/cwwed.service.ts
+++ frontend/src/app/cwwed.service.ts
@@ -1,11 +1,8 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { environment } from '../environments/environment';
 
-let API_ROOT_DEV = '/api';
-let API_ROOT_PROD = 'https://dev.cwwed-staging.com/api';
-let API_ROOT = environment.production ? API_ROOT_PROD : API_ROOT_DEV;
+let API_ROOT = '/api';
 let API_USER = `${API_ROOT}/user/`;
 let API_COVERED_DATA = `${API_ROOT}/covered-data/`;
 let API_NAMED_STORMS = `${API_ROOT}/named-storms/`;
