427716e6bbb4655a81308e3981691e1788bf3619
danny flack <flackattack@gmail.com>
2019-08-21 09:57:37 -0500

    subset shapefile by requested bbox too

--- named_storms/tasks.py
+++ named_storms/tasks.py
@@ -325,8 +325,6 @@ def email_nsem_covered_data_complete_task(nsem_data: dict, base_url: str):
 
 @app.task(**TASK_ARGS)
 def create_psa_user_export_task(nsem_psa_user_export_id: int):
-    # TODO - this is a proof of concept and is only working with the water dataset
-    #        it assumes gridded with specific variables
     nsem_psa_user_export = get_object_or_404(NsemPsaUserExport, id=nsem_psa_user_export_id)
     date_expires = pytz.utc.localize(datetime.utcnow()) + timedelta(days=settings.CWWED_PSA_USER_DATA_EXPORT_DAYS)
 
@@ -346,6 +344,9 @@ def create_psa_user_export_task(nsem_psa_user_export_id: int):
 
     # netcdf/csv - extract low level data from netcdf files
     if nsem_psa_user_export.format in [NsemPsaUserExport.FORMAT_NETCDF, NsemPsaUserExport.FORMAT_CSV]:
+
+        # TODO - this is a proof of concept and is only working with the water dataset
+        #        it assumes gridded with specific variables
 
         for ds_file in os.listdir(psa_path):
 
@@ -373,6 +374,7 @@ def create_psa_user_export_task(nsem_psa_user_export_id: int):
 
     # shapefile - extract pre-processed contour data from db
     elif nsem_psa_user_export.format == NsemPsaUserExport.FORMAT_SHAPEFILE:
+
         # generate shapefiles for all polygon variables
         for psa_geom_variable in nsem_psa_user_export.nsem.nsempsavariable_set.filter(geo_type=NsemPsaVariable.GEO_TYPE_POLYGON):
 
@@ -384,21 +386,26 @@ def create_psa_user_export_task(nsem_psa_user_export_id: int):
             # GeoPandas, but django doesn't produce valid sql due to it not quoting params (specifically dates), so this
             # technique is a workaround
 
-            # fetch the ids of the data we want
-            kwargs = {
-                'nsem_psa_variable__id': psa_geom_variable.id,
-            }
+            # fetch the ids of the psa data for this psa variable and that intersects the export's requested bbox
+            kwargs = dict(
+                nsem_psa_variable__id=psa_geom_variable.id,
+                geo__intersects=nsem_psa_user_export.bbox,
+            )
+
             # only include date if it's a time series variable
             if psa_geom_variable.data_type == NsemPsaVariable.DATA_TYPE_TIME_SERIES:
                 kwargs['date'] = nsem_psa_user_export.date_filter
+
             qs = NsemPsaData.objects.filter(**kwargs)
             data_ids = [r['id'] for r in qs.values('id')]
 
-            # generate raw sql query to send to geopanda's GeoDataFrame
+            # generate raw sql query to send to geopanda's GeoDataFrame.from_postgis()
             qs = NsemPsaData.objects.annotate(geom=Cast('geo', CharField()))
             qs = qs.filter(id__in=data_ids)
             qs = qs.values('geom', 'value')
             gdf = GeoDataFrame.from_postgis(str(qs.query), connection, geom_col='geom')
+
+            # save to temporary user path
             gdf.to_file(os.path.join(tmp_user_export_path, '{}.shp'.format(psa_geom_variable.name)))
 
     # create tar in local storage
