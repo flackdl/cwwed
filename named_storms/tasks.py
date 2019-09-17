b637646f720fa668b22bd178764d1cee20a6d710
danny flack <flackattack@gmail.com>
2019-09-17 15:29:34 -0500

    only return export bbox intersection for shapefile too

--- named_storms/tasks.py
+++ named_storms/tasks.py
@@ -431,7 +431,7 @@ def create_psa_user_export_task(nsem_psa_user_export_id: int):
         for psa_geom_variable in nsem_psa_user_export.nsem.nsempsavariable_set.filter(**kwargs_polygon_time_series):
 
             #
-            # generate sql query to send to geopanda's GeoDataFrame to create a shapefile
+            # generate sql query to send to geopanda's GeoDataFrame.from_postgis() to create a shapefile
             #
 
             # NOTE: we have to fetch all the ids of the actual data up front because we need to send the raw query to
@@ -451,8 +451,11 @@ def create_psa_user_export_task(nsem_psa_user_export_id: int):
             qs = NsemPsaData.objects.filter(**kwargs)
             data_ids = [r['id'] for r in qs.values('id')]
 
-            # generate raw sql query to send to geopanda's GeoDataFrame.from_postgis()
-            qs = NsemPsaData.objects.annotate(geom=Cast('geo', CharField()))
+            # group all intersecting geometries together by variable & value and
+            # only return the export's bbox intersection
+            # also, it's necessary to finally cast to CharField for GeoPandas
+            qs = NsemPsaData.objects.annotate(
+                geom=Cast(Intersection(Collect(Cast('geo', GeometryField())), nsem_psa_user_export.bbox), CharField()))
             qs = qs.filter(id__in=data_ids)
             qs = qs.values('geom', 'value')
             gdf = GeoDataFrame.from_postgis(str(qs.query), connection, geom_col='geom')
