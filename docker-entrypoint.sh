e0018f0ff82bc663ce2475de837967b7860317ae
danny <flackattack@gmail.com>
2020-03-01 16:50:16 -0600

    rm ipv6...again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,4 @@ python manage.py migrate
 python manage.py createcachetable
 
 # run application on ipv4 and ipv6
-gunicorn cwwed.wsgi --worker-class gthread --workers 4 --threads 4 --bind 127.0.0.1:80 --bind [::1]:80
+gunicorn cwwed.wsgi --worker-class gthread --workers 4 --threads 4 --bind 127.0.0.1:80
