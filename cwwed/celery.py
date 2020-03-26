3a8bb5fb665c3ebbf5525a3caeb9d5b302b89b39
danny <flackattack@gmail.com>
2020-03-26 11:32:03 -0500

    swap celery broker to redis

--- cwwed/celery.py
+++ cwwed/celery.py
@@ -7,8 +7,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cwwed.settings')
 
 app = Celery(
     'cwwed',
-    broker='amqp://{}'.format(os.environ.get('CELERY_BROKER', '')),
-    backend='rpc://{}'.format(os.environ.get('CELERY_BACKEND', '')),
+    broker='redis://{}'.format(os.environ.get('CELERY_BROKER', '')),
+    backend='rpc://',
     include=['named_storms.tasks'],
 )
 
