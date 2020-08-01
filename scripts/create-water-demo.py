1ad191152521dcca0a34f94131b6bb89968632d1
danny <flackattack@gmail.com>
2020-08-01 11:35:20 -0500

    update water demo script

--- scripts/create-water-demo.py
+++ scripts/create-water-demo.py
@@ -7,29 +7,47 @@ wave_height_src_path = os.path.join(base_path, 'HS_WND_STR_SUBSET.nc')
 water_level_src_path = os.path.join(base_path, 'WATERLEVEL_CURRENT_STR_SUBSET.nc')
 out_path = os.path.join(base_path, 'water-demo.nc')
 
-dates = pd.date_range(start='2018-09-14T01:00:00', end='2018-09-14T20:00:00', freq='1H')
-
-# remove any existing path if it exists
+# remove existing output path if it exists
 if os.path.exists(out_path):
     os.remove(out_path)
 
+# dates to use to mirror the wind dataset
+dates = pd.date_range(start='2018-09-14T01', end='2018-09-14T20', freq='1H', tz='UTC')
+
 # open datasets
 ds_wave = xr.open_dataset(wave_height_src_path)
-ds_water = xr.open_dataset(water_level_src_path)
+# this one has invalid CF times (we'll decode afterwards)
+ds_water = xr.open_dataset(water_level_src_path, decode_times=False)
 
-# map values to be on the hour
-wave_height = ds_wave['hs'].interp(time=dates)
-water_level = ds_water['zeta'].interp(time=dates)
-
-# water level max doesn't have a time
+# select wave and water level max
+wave_height = ds_wave['hs'].sel(time=dates)
 water_level_max = ds_water['zeta_max']
 
-# create a new dataset with the hourly dates
+# since the water dataset has invalid seconds-since, we define the base date here
+water_base_date = pd.to_datetime(ds_water.time.attrs['base_date'], format='%d-%b-%Y %H:%M:%S %Z')
+
+# define the start and end times to filter
+min_time = (dates[0] - water_base_date).total_seconds()
+max_time = (dates[-1] - water_base_date).total_seconds()
+
+# filter time and water level to the desired dates
+water_level = ds_water.zeta[(ds_water.time <= max_time) & (ds_water.time >= min_time)]
+times = ds_water.time[(ds_water.time <= max_time) & (ds_water.time >= min_time)]
+
+# redefine water dataset with updated values and decode cf values
+ds_water = xr.Dataset({"water_level": water_level, "time": times})
+ds_water = xr.decode_cf(ds_water)
+
+# create a new dataset with the variables
 ds_out = xr.Dataset({
-    'wave_height': xr.DataArray(wave_height, coords=[dates, ds_wave.latitude, ds_wave.longitude], dims=["time", "lat", "lon"]),
-    'water_level': xr.DataArray(water_level, coords=[dates, ds_water.latitude, ds_water.longitude], dims=["time", "lat", "lon"]),
+    'wave_height': xr.DataArray(wave_height, coords=[times, ds_wave.latitude, ds_wave.longitude], dims=["time", "lat", "lon"]),
+    'water_level': xr.DataArray(ds_water['water_level'], coords=[times, ds_water.latitude, ds_water.longitude], dims=["time", "lat", "lon"]),
     'water_level_max': xr.DataArray(water_level_max, coords=[ds_water.latitude, ds_water.longitude], dims=["lat", "lon"]),
 })
+
+# update time to standard attributes
+ds_out['time'].attrs['units'] = 'seconds since {}'.format(water_base_date)
+ds_out['time'].attrs['base_date'] = water_base_date.isoformat()
 
 print('Saving to {}'.format(out_path))
 
