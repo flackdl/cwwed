9f315f0a111089a5ff0475aca200a07f31df456c
danny <flackattack@gmail.com>
2021-03-01 13:42:04 -0600

    always run cwwed init during redeploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ python manage.py migrate
 # create cache tables
 python manage.py createcachetable
 
+# run cwwed init
+python manage.py cwwed-init
+
 # run application on ipv4 and ipv6
 gunicorn cwwed.wsgi --max-requests 1000 --worker-class gthread --workers 4 --threads 4 --bind [::]:80
